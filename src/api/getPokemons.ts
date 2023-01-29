c8b4779c8818b50a62ff92644fa6e57a834b16d0
AnaCatanaR <anacatana.ac@gmail.com>
2023-01-29 15:49:39 +0100

    Add function getPokemons

---
+++ src/api/getPokemons.ts
@@ -0,0 +1,16 @@
+import type { PokemonsList } from "../ts/types.js";
+
+const pokemonUrlApi = "https://pokeapi.co/api/v2/";
+
+export const getPokemons = async (
+  limitCounter: number,
+  offsetCounter: number
+) => {
+  const pokemonsPathList =
+    pokemonUrlApi + `pokemon?limit=${limitCounter}&offset=${offsetCounter}`;
+
+  const response = await fetch(pokemonsPathList);
+  const listResponse = (await response.json()) as PokemonsList;
+
+  return listResponse;
+};
--- src/ts/types.ts
+++ src/ts/types.ts
@@ -2,3 +2,26 @@ export interface ComponentStructure {
   element: Element;
   render: () => void;
 }
+
+export interface Pokemon {
+  name: string;
+  id: number;
+  types: Type[];
+  sprites: {
+    front_default: string;
+  };
+}
+
+export interface Type {
+  type: {
+    name: string;
+  };
+}
+
+export interface PokemonsList {
+  results: [
+    {
+      name: string;
+    }
+  ];
+}
